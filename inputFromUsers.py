41448f460deb2c6aaeef5ee618d4591601bbd62d
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-09-09 14:41:52 -0400

    grabbing input

---
+++ inputFromUsers.py
@@ -0,0 +1,10 @@
+# Getting input from users
+# For python 3, it will be input("Enter your name")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Python 2, uses raw_input()
+# name = raw_input("Enter your name: ")
+
+
+print("Hello " + name + ". You are " + age)
