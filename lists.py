6eba77cf1a3a696d262a1aa287534e6ae6981cec
Brian Lafuente <loulafuente@MacBook-Pro.local>
2019-09-09 17:32:20 -0400

    updated madlibs

---
+++ lists.py
@@ -0,0 +1,20 @@
+# creating a list
+friends = ["Des", "Jam", "Bea", "Lena"]
+
+# You can put any kind of data type in the list/array
+
+print(friends)
+
+# grabbing a certain name depending on the index
+print(friends[1])
+ 
+# Access elements from back of the list
+print(friends[-2])
+
+
+# selecting the certain elements up to 
+print(friends[1:])
+
+# change an element
+friends[1] = "Karen"
+print(friends)
--- madlibs.py
+++ madlibs.py
@@ -3,9 +3,16 @@
 # create variables
 color = input("Enter a color: ")
 pluralNoun = input("Enter a plural noun: ")
-celebrity = input("Enter a celebrity name: ")
+celebrity = input("Enter a celebrity name: ") 
 
 # print out the results
 print("Roses are " + color)
 print(pluralNoun + " are blue")
 print("I love " + celebrity)
+
+message = """
+Roses are %s
+%s are blue
+I love %s
+""" % (color,pluralNoun,celebrity)
+print(message)
