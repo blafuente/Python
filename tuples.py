bdcef173ab039a4a9a4cac63bb6f7be1e6ad7ab2
Brian Lafuente <loulafuente@MacBook-Pro.local>
2019-09-09 18:11:44 -0400

    tuples init commit

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# Tuples
+# Tuple is a type of data structer which you can store values
+# it is similar to lists/arrays but has key differences
+
+# To creat a tuple
+# use () to creat tuples
+coordinates = (4, 5)
+# tuple is immutable, tuple CANNOT BE CHANGE OR MODIFIED
+# Cannot edit, change, add, or remove elements
+
+print(coordinates[0])
+
+coordinates = [(4, 5), (6, 7), (80, 34)]
+print(coordinates)
