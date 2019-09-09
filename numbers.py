ead2cabb5fbc0ba3dc56e9c012839727f5ed2ed0
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-09-09 14:29:28 -0400

    finished numbers lesson

--- numbers.py
+++ numbers.py
@@ -1,3 +1,59 @@
 # Numbers
 # Numbers are very common when using Python
 
+#you can import math functions
+from math import *
+# It grabs a bunch more math functions to access them 
+
+print(11)
+print(2.0987)
+print(-2.3423)
+
+# can also do arithmetic 
+print(3 + 4.5)
+print(3 - 4.5)
+print(3 * 4.5)
+print(3 / 4.5)
+
+# order of operations and paranthesis
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+
+# modular operator
+# prints the remainder as the result
+print(10 % 3) 
+
+# can store numbers in variables
+my_num = 5
+print(my_num)
+
+# converting number into a string str()
+print(str(my_num))
+print(str(my_num) + " my favorite number")
+
+# absolute value
+my_num = -5
+print(abs(my_num))
+
+# power to, exponential
+print(pow(3, 2))
+print(pow(4, 6))
+
+# max
+print(max(4, 6))
+
+# min 
+print(min(4, 6))
+
+# round function, round a number 
+print(round(3.2))
+print(round(3.7))
+
+# floor method, grab the lowest number
+print(floor(3.7))
+
+# ceiling method, round to the next highest value
+print(ceil(3.7))
+
+# Square Root a value
+print(sqrt(36))
