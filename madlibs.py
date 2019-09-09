95de19adfb202a8c476d84d0d44619cb2131d9e3
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-09-09 14:54:02 -0400

    mad libs game

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# Creating a madlibs game
+
+# create variables
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity name: ")
+
+# print out the results
+print("Roses are " + color)
+print(pluralNoun + " are blue")
+print("I love " + celebrity)
