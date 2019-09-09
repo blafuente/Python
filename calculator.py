8593fdec16278a3c86585185da06514ba5a9607d
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-09-09 14:49:33 -0400

    basic addition calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,12 @@
 # creating a calculator
 
+# create the variables to store users input
 num1 = input("Enter a number: ")
-num2 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+
+print(result)
+
+# int() function gives whole numbers
+# float() works with decimal numbers
+
