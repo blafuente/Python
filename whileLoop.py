862f1846626f8ecaa2a8b452fb6fbbd8a5323dd6
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-09-10 16:13:07 -0400

    while loop init

---
+++ whileLoop.py
@@ -0,0 +1,10 @@
+# While Loops
+
+i = 1
+# while condition == True, keep looping the code
+while i <= 10:
+    print(i)
+    i += 1
+# once the condition is False
+# in this case, once i == 11, the loop breaks or stops
+print("Done with loop")
