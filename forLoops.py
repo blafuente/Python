113cadcca91ea98608ff0ef60b00b4769009e855
Brian Lafuente <loulafuente@MacBook-Pro.attlocal.net>
2019-09-10 20:02:37 -0400

    sep 10

---
+++ forLoops.py
@@ -0,0 +1,14 @@
+# For Loops 
+
+for letter in "Giraffe Academy":
+    # Loops thru the string and spits out each letter
+    print(letter)
+
+friends = ["Des", "Jam", "Bea"]
+for friend in friends:
+    print(friend)
+    # Loops thru the array and prints out each element in the array 
+
+# A loop of numbers
+for index in range(1,11):
+    print index
